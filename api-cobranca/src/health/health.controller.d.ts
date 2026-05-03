4279df03d2dcb899ffcd9edf662492bee2d8068f
Vitor Pereira <pereira.vitor@edu.fecap.br>
2026-05-03 20:50:28 -0300

    remover arquivos

--- api-cobranca/src/health/health.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Response } from 'express';
-import { HealthService } from './health.service';
-export declare class HealthController {
-    private readonly service;
-    private readonly logger;
-    constructor(service: HealthService);
-    get(res: Response): Promise<Response>;
-}
--- api-cobranca/src/health/indicators/evolution.indicator.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import type { HealthCheckResult } from '../types';
-export declare class EvolutionHealthIndicator {
-    private readonly url;
-    constructor(config: ConfigService);
-    check(): Promise<HealthCheckResult>;
-}
